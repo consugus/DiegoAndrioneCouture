cc9f99368f28c9d89a90046b1156d0e6d2ea5d91
Gustavo <consugus@gmail.com>
2019-02-10 21:15:58 -0300

    Bar fixed on top after scrolling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
     console.log("se cargó correctamente jQuery");
 
+    var windowHeight = $(window).height();
+    console.log("Window Heigth: " + windowHeight);
+
+    var windowWidth = $(window).width();
+    console.log("Window Width: " + windowWidth);
+
+    $(window).scroll(function () {
+
+        var scrollTop = $(window).scrollTop();
+        var barraHeight = $(".barra").innerHeight();
+
+        // console.log(scrollTop);
+        if(scrollTop > (windowHeight-60) ){
+            $('#bar').addClass("fixed");
+            $("body").css({"margin-top": barraHeight + "px"});
+        } else{
+            $(".bar").removeClass("fixed");
+            $("body").css({"margin-top": "0px"});
+        };
+    });
 
 
 
