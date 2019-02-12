33ff0f4d31977d798b4ddd57cf0edc1a6bc1a978
Gustavo <consugus@gmail.com>
2019-02-12 11:07:53 -0300

    Transitions improved, scroll smooth, sections "Sobre mi trabajo" y "Galería" added

--- js/main.js
+++ js/main.js
@@ -32,6 +32,16 @@ $(document).ready(function(){
         pager: true
     });
 
+    // Smooth scroll
+    $("a[href^='#']").click(function(e) {
+        e.preventDefault();
+
+        var position = $($(this).attr("href")).offset().top;
+
+        $("body, html").animate({
+            scrollTop: position
+        }, 2000 );
+    });
 
 
 });
